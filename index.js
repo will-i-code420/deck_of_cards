dfd13a7bf20dddfca9c7c1291b6889b0205ba354
Will Aronow <waronow123@gmail.com>
2021-11-11 14:44:55 -0700

    added shuffle function and refactored drawCards

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-function createDeck() {
-	const suits = [ 'hearts', 'diamonds', 'spades', 'clubs' ];
-	const values = [ '2', '3,', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
-	const deck = [];
-	for (const value of values) {
-		for (const suit of suits) {
-			deck.push({ value, suit });
+const playerDeck = {
+	suits: [ 'hearts', 'diamonds', 'spades', 'clubs' ],
+	values: [ '2', '3,', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ],
+	deck: [],
+	hand: [],
+	createDeck() {
+		const { suits, values, deck } = this;
+		for (const value of values) {
+			for (const suit of suits) {
+				deck.push({ value, suit });
+			}
 		}
+		this.shuffleDeck();
+	},
+	shuffleDeck() {
+		const { deck } = this;
+		let currentIdx = deck.length;
+		let tempCard, randomIdx;
+		while (0 !== currentIdx) {
+			randomIdx = Math.floor(Math.random() * currentIdx);
+			currentIdx--;
+			tempCard = deck[currentIdx];
+			deck[currentIdx] = deck[randomIdx];
+			deck[randomIdx] = tempCard;
+		}
+	},
+	drawCards(num) {
+		const { deck, hand } = this;
+		hand.push(deck.splice(0, num));
 	}
-	return deck;
-}
-
-function drawCard(cards) {
-	const idx = Math.floor(Math.random() * cards.length);
-	const card = cards.splice(idx, 1);
-	return card;
-}
-
-const deck = createDeck();
+};
