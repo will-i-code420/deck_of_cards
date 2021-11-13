176a3629d40fb9c553b513df1da79c9518414d8f
Will Aronow <waronow123@gmail.com>
2021-11-13 13:50:52 -0700

    made cards dynamic and start with 5 cards on new game

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const playerDeck = {
-	suits: [ 'hearts', 'diamonds', 'spades', 'clubs' ],
-	values: [ '2', '3,', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ],
+	suits: [ 'heart', 'diamond', 'spade', 'club' ],
+	values: [ '1', '2', '3,', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king' ],
 	deck: [],
 	hand: [],
 	createDeck() {
@@ -23,16 +23,95 @@ const playerDeck = {
 			deck[currentIdx] = deck[randomIdx];
 			deck[randomIdx] = tempCard;
 		}
+		this.drawCards();
 	},
-	drawCards(num) {
+	drawCards() {
+		const drawAmt = document.querySelector('.draw-amt');
 		const { deck, hand } = this;
-		hand.push(deck.splice(0, num));
+		if (!hand.length) {
+			hand.push(deck.splice(0, 5));
+		} else {
+			hand.push(deck.splice(0, drawAmt.valueAsNumber));
+			drawAmt.value = '';
+		}
+		this.createCardContainers();
+	},
+	createCardContainers() {
+		const numOfCards = this.hand[0].length;
+		const cardContainer = document.querySelector('#card-container-columns');
+		for (let i = 0; i < numOfCards; i++) {
+			const card = document.createElement('div');
+			if (i === 0) {
+				card.classList.add('column', 'card', 'is-2', 'is-offset-1');
+			} else {
+				card.classList.add('column', 'card', 'is-2');
+			}
+
+			const cardImg = document.createElement('div');
+			cardImg.classList.add('card-image');
+			const figure = document.createElement('div');
+			figure.classList.add('image');
+			cardImg.appendChild(figure);
+			card.appendChild(cardImg);
+			cardContainer.appendChild(card);
+		}
+		this.displayCardImgs();
+	},
+	displayCardImgs() {
+		const { hand } = this;
+		const cards = document.querySelectorAll('.image');
+		cards.forEach((card, i) => {
+			const suit = hand[0][i].suit;
+			const value = hand[0][i].value;
+			const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+			svg.setAttribute('viewBox', '0 0 300 300');
+			svg.setAttribute('height', '300');
+			svg.setAttribute('width', '300');
+			const svgContent = `<use href="./images/cards/svg-cards/svg-cards.svg#${suit}_${value}" x="0" y="0"/>`;
+			svg.innerHTML = svgContent;
+			card.appendChild(svg);
+		});
+	},
+	clearCurrentHand() {
+		this.hand = [];
+	},
+	clearDeck() {
+		this.deck = [];
+		this.hand = [];
 	}
+};
+
+const checkBtnDisable = (btns) => {
+	btns.forEach((btn) => {
+		if (btn.disabled) {
+			btn.disabled = false;
+		} else {
+			btn.disabled = true;
+		}
+	});
 };
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((btn) => {
 	btn.addEventListener('click', (e) => {
-		console.log(e.target.value);
+		switch (e.target.value) {
+			case 'start':
+				playerDeck.createDeck();
+				checkBtnDisable(buttons);
+				break;
+			case 'end':
+				playerDeck.clearDeck();
+				checkBtnDisable(buttons);
+				break;
+			case 'draw':
+				playerDeck.drawCards();
+				break;
+			case 'fold':
+				playerDeck.clearCurrentHand();
+				break;
+
+			default:
+				break;
+		}
 	});
 });
